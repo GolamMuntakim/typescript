f55a2f3aba9f888b1793010d7e69ce124729d478
Golam Muntakim <seams123456789@gmail.com>
2024-09-17 23:17:08 -0700

    rest operator in typescript

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,37 @@
 //     weight : 345, 
   
 // }
+
+// function in typescript
+// type FuncType = (n:number , m:number) => number
+// const func:FuncType = (a,b) =>{
+//     return a*b;
+// };
+// console.log(func(2,3))
+
+//parameter 
+// type FuncType = (n:number , m:number ,l?:number) => number ;
+// const func:FuncType = (n,m,l=20) =>{
+//     return n*m*l;
+// }
+//  func(25,23);
+
+
+ // Rest operator 
+//  type FuncType = (...m : number[]) => number[];
+//  const func:FuncType = (...m) =>{
+//     return m
+//  }
+//  func(23,2,344,5,5,4)
+
+
+//fuction with object 
+interface product { name:string, stock: number, price:number }
+type GetDataTYpe = (product:product)=> void
+const getData:GetDataTYpe = (product) => {
+console.log(product)
+}
+const productOne:product = {
+    name:"Mackbook", price:46, stock:45
+}
+getData(productOne)
