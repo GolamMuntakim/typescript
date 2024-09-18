60e245e453e14d7c7278c28ae2e9e754aa4f51d1
Golam Muntakim <seams123456789@gmail.com>
2024-09-17 21:12:54 -0700

    object in typescript

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//object in typescript
+// interface Obj  {
+//     height : number;
+//     weight : number ;
+//     gender? : boolean;
+// };
+// const obj:Obj = {
+//     height : 434,
+//     weight : 345,
+// }
+// const obj2:Obj = {
+//     height : 434,
+//     weight : 345, 
+// }
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+//object in typescript
+// interface Obj  {
+//     height : number;
+//     weight : number ;
+//     gender? : boolean;
+// };
+// const obj:Obj = {
+//     height : 434,
+//     weight : 345,
+   
+// }
+// const obj2:Obj = {
+//     height : 434,
+//     weight : 345, 
+  
+// }
