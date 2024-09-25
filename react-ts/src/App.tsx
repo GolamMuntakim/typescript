953e4e46a1605d0c7d1a8f772e3f6f1213839743
Golam Muntakim <seams123456789@gmail.com>
2024-09-25 11:18:29 -0700

    full useState hook

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,24 @@
-import { useState } from "react"
-import Box from "./Component/Box"
+import { FormEvent, useState } from "react"
 
+interface Person {
+  name : string,
+  age : number
+}
 
 function App() {
-  const [value, setValue] = useState<string>("")
+  const [user, setUser] = useState<Person>()
+  const submitHandler = (e:FormEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+    console.log(user)
+  }
   return (
     <>
       <div>
-      <Box label="Search" value={value} setter={setValue} >
-       
-      </Box>
+    <form onSubmit={submitHandler}>
+      <input type="number" placeholder="age" value={user?.age || ""} onChange={e=> setUser(prev=>({...prev!, age:Number(e.target.value)}))} />
+      <input type="text" placeholder="name" value={user?.name || ""} onChange={e=> setUser(prev=>({...prev!, name:e.target.value}))} />
+      <button type="submit">Submit</button>
+    </form>
       </div>
      
     </>
